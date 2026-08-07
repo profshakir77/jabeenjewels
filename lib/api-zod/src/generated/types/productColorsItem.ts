19936c54e5d205fdeaddceadf015429d1f7971d6
Shakir Hussain <prof.shakir77@gmail.com>
2026-08-07 18:05:02 +0500

    Add nullable image property to ProductColorsItem type

--- lib/api-zod/src/generated/types/productColorsItem.ts
+++ lib/api-zod/src/generated/types/productColorsItem.ts
@@ -5,8 +5,9 @@
  * Jabeen Jewels e-commerce API
  * OpenAPI spec version: 0.1.0
  */
-
 export type ProductColorsItem = {
   name?: string;
   quantity?: number;
+  /** @nullable */
+  image?: string | null;
 };
